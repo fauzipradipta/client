f116b2f0949c5402057399da8ee5573ef83f4f2d
Syawadhilah Pradipta <dipta0988@gmail.com>
2022-06-02 16:48:02 -0700

    update with jwt_token

--- src/app/shared/interceptor/index.ts
+++ src/app/shared/interceptor/index.ts
@@ -1,6 +1,7 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorInterceptor } from './error.interceptor';
 import { HeaderInterceptor } from './header.interceptor';
+import { TokenInterceptor } from './token.interceptor';
 export const httpInterceptor = [
   {
     //this service it is an imp of http intercept angular will take care for you.
@@ -16,6 +17,14 @@ export const httpInterceptor = [
     //injecting these interceptore as a service
 
     useClass: ErrorInterceptor,
+    multi: true,
+  },
+  {
+    //this service it is an imp of http intercept angular will take care for you.
+    provide: HTTP_INTERCEPTORS,
+    //injecting these interceptore as a service
+
+    useClass: TokenInterceptor,
     multi: true,
   },
 ];
---
+++ src/app/shared/interceptor/token.interceptor.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    //retrieve the token from local storage
+    let token = localStorage.getItem('token');
+    console.log(jwt_decode(token || ''));
+
+    let decoded: any = jwt_decode(token || '');
+    console.log(decoded);
+    //except login and register we have to add token for every rest api request
+
+    // before add the token:===> we should validate the token by extracting expiry of the tokens
+
+    //then we should redirect to the login page
+
+    if (request.url.includes('login') || request.url.includes('register')) {
+      console.log('register/login detected');
+      return next.handle(request);
+    }
+
+    //if there is no token we should redirect to the login page
+    if (token == null || token.length == 0) {
+      this.router.navigate(['/users/login']);
+    }
+
+    return next.handle(request);
+  }
+}
