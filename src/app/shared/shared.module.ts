49e365c8008f798832eb875b1db84a0936dab608
Syawadhilah Pradipta <dipta0988@gmail.com>
2022-06-07 13:08:02 -0700

    added directives and pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,14 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderInterceptor } from './interceptor/header.interceptor';
 import { httpInterceptor } from './interceptor'; //it  will automactically refer  to index.ts file
 import { AuthGuard } from './guard/auth.guard';
+import { UppercasePipe } from './pipes/uppercase.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UppercasePipe,
+    HighlightDirective
+  ],
   imports: [CommonModule],
   providers: [httpInterceptor, AuthGuard],
 })
